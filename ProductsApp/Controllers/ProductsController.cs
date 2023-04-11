11766a61255d2feca4b50721d03595c017bbe11f
tui79500 <tui79500@temple.edu>
2023-04-11 14:31:37 -0400

    Changed The Products

--- ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/Controllers/ProductsController.cs
@@ -11,9 +11,9 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "Printer Paper", Category = "Office Supplies", Price = 3.49M },
+            new Product { Id = 2, Name = "Nike Shoes", Category = "Clothing", Price = 54.99M },
+            new Product { Id = 3, Name = "Doritos", Category = "Snacks", Price = 16.99M }
         };
 
         public ProductsController()
